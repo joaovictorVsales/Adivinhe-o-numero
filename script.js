b71db73385be1cb091e7713f64617e94a3bb36fe
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-08-22 01:25:54 -0300

    Adição de tentativas e fim do looping

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const numA = Math.floor(Math.random() * 21);
+numA = Math.floor(Math.random()*21);
 win = false;
+let tentativa = 0;
 
-while(!win){
-function adivinhar(){
-    let input = Number(prompt("Digite um número entre 1 e 20: "));
+while(!win && tentativa < 10){
+    let input = Number(prompt("Digite um número de um a vinte: "))
+    tentativa++;
 
-    if (input === numA) {
-        console.log("Acertou!!!");
-        win = true
-    } 
-    else if (input < numA) {
-        console.log("Errou! O número é maior");
-    } 
-    else if (input > numA) {
-        console.log("Errou! O número é menor");
-    }
-}
+if(input < 0 || input > 20){
+    console.log ("Digite um número entre UM e VINTE")
 }
 
+if(input === numA){
+    console.log (`Acertou!!! tentativa:  ${tentativa}/10`)
+    break;
+}
 
-adivinhar();
+else if(input > numA){
+    console.log (`Errou!!! o número é menor tentativa:  ${tentativa}/10`)
+}
+
+else if(input < numA){
+    console.log (`Errou!!! o número é maior tentativa:  ${tentativa}/10`)
+}
+if(tentativa === 10 && !win){
+    console.log (`PERDEU!!! o número era ${numA}`)
+    break;
+}
+
+}
+
