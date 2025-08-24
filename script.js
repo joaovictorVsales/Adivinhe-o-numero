c85f1926ff248b1c1d397c4f03aef2783adfcef7
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-08-24 20:57:55 -0300

    Linkando ao  html

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-numA = Math.floor(Math.random()*21);
-win = false;
+let numA = Math.floor(Math.random() * 21);
+let win = false;
 let tentativa = 0;
 
-while(!win && tentativa < 10){
-    let input = Number(prompt("Digite um número de um a vinte: "))
+document.getElementById("Confirmar").addEventListener("click", function() {
+    let input = parseInt(document.getElementById("box").value);
     tentativa++;
 
-if(input < 0 || input > 20){
-    console.log ("Digite um número entre UM e VINTE")
-}
+    if (input < 1 || input > 20) {
+        console.log("Digite um número entre UM e VINTE");
+        return;
+    }
 
-if(input === numA){
-    console.log (`Acertou!!! tentativa:  ${tentativa}/10`)
-    break;
-}
+    if (input === numA) {
+        console.log(`Acertou!!! tentativa: ${tentativa}/10`);
+        win = true;
+        return;
+    } else if (input > numA) {
+        console.log(`Errou!!! o número é menor tentativa: ${tentativa}/10`);
+    } else if (input < numA) {
+        console.log(`Errou!!! o número é maior tentativa: ${tentativa}/10`);
+    }
 
-else if(input > numA){
-    console.log (`Errou!!! o número é menor tentativa:  ${tentativa}/10`)
-}
-
-else if(input < numA){
-    console.log (`Errou!!! o número é maior tentativa:  ${tentativa}/10`)
-}
-if(tentativa === 10 && !win){
-    console.log (`PERDEU!!! o número era ${numA}`)
-    break;
-}
-
-}
-
+    if (tentativa === 10 && !win) {
+        console.log(`PERDEU!!! o número era ${numA}`);
+        return;
+    }
+});
