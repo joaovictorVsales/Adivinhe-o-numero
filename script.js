c40e70ee19efe419a8f7d78430d08f6b76149946
JVVS <163188697+joaovictorVsales@users.noreply.github.com>
2025-08-25 14:21:47 -0300

    Finalização

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ let numA = Math.floor(Math.random() * 21);
 let win = false;
 let tentativa = 0;
 
-document.getElementById("Confirmar").addEventListener("click", function() {
+document.getElementById("confirmar").addEventListener("click", function() {
     let input = parseInt(document.getElementById("box").value);
+    let mensagem = document.getElementById("mensagem")
     tentativa++;
 
     if (input < 1 || input > 20) {
-        console.log("Digite um número entre UM e VINTE");
+        mensagem.textContent ="Digite um número entre UM e VINTE";
         return;
     }
 
     if (input === numA) {
-        console.log(`Acertou!!! tentativa: ${tentativa}/10`);
+        mensagem.textContent=`Acertou!!! tentativa: ${tentativa}/10`;
         win = true;
         return;
     } else if (input > numA) {
-        console.log(`Errou!!! o número é menor tentativa: ${tentativa}/10`);
+        mensagem.textContent=`Errou!!! o número é menor tentativa: ${tentativa}/10`;
     } else if (input < numA) {
-        console.log(`Errou!!! o número é maior tentativa: ${tentativa}/10`);
+        mensagem.textContent=`Errou!!! o número é maior tentativa: ${tentativa}/10`;
     }
 
     if (tentativa === 10 && !win) {
-        console.log(`PERDEU!!! o número era ${numA}`);
+        mensagem.textContent=`PERDEU!!! o número era ${numA}`;
         return;
     }
 });
